5f429e659212da12726b88cca7af90ff52b9c6a8
a-stone-arachnid <31635665+a-stone-arachnid@users.noreply.github.com>
2019-05-05 07:49:00 -0700

    Create main.m

---
+++ src/main.m
@@ -0,0 +1,11 @@
+//
+//  main.m
+//  CabbageScript Editor
+//
+//  Copyright (c) 2019 SNDC. All rights reserved.
+//
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
